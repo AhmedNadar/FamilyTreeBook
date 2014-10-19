7d5b242464f8939e128d87aae64be2b6796188f9
Ahmed Nadar <ahmedfay@hotmail.com>
2014-10-19 05:32:00 -0400

    Add custome routes for Auth and feeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :statuses
-  root to: "statuses#index"
 
+  devise_scope :user do
+	  get "login", to: "devise/sessions#new", as: :login
+	  get "join", to: "registrations#new", as: :join
+	  get "logout", to: "devise/sessions#destroy", as: :logout
+	  get "settings", to: "registrations#edit", as: :settings
+	end
+
+  resources :statuses
+  get "feeds", to: "statuses#index", as: :feeds
+  root to: "statuses#index"
 end
