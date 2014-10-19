5d11513aecde59b9e179fdb87041d6a6a000c458
Ahmed Nadar <ahmedfay@hotmail.com>
2014-10-18 21:50:16 -0400

    Validates user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # associations
   has_many :statuses
+  
+  
+  # Validations
+  validates_presence_of :first_name, :last_name, :profile_name
+	validates_uniqueness_of :profile_name
+  validates_format_of :profile_name, with: /\A[a-zA-Z0-9_-]+\Z/, message: "Must be formatted correctly."
+
 
   def full_name
   	"#{first_name.capitalize} #{last_name.capitalize}"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,41 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.first_name = users(:ahmed).first_name
+    user.last_name = users(:ahmed).last_name
+    user.profile_name = users(:ahmed).profile_name
+    user.email = users(:ahmed).email
+    user.password = "password"
+		user.password_confirmation = "password"
+		assert !user.save
+		# puts user.errors.inspect
+		assert !user.errors[:profile_name].empty?
+	end
+
+  test "a user should enter a profile name without spaces" do
+    user = User.new
+    user.profile_name = "ahmed nadar with spaces"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
