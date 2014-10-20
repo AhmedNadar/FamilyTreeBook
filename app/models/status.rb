67d916540326f637e938eae1efa77439db1d988b
Ahmed Nadar <ahmedfay@hotmail.com>
2014-10-20 14:29:06 -0400

    Tested, Logged in user should create a status

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,7 @@
 class Status < ActiveRecord::Base
 	belongs_to :user
+
+	validates_presence_of :content
+	validates_length_of :content, minimum: 4
+	validates_presence_of :user_id
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "a user should enter a content for status" do
+		status = Status.new
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+
+	test "status length should be more than 3 letters" do
+		status = Status.new
+		status.content = "hey"
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+
+	test "that status should has a user id" do 
+	  status = Status.new
+	  status.content = "Howdy"
+	  assert !status.save
+	  assert !status.errors[:user_id].empty?
+	end
 end
