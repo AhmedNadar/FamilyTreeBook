9100f212797618653c9b3f9edf532d4ef0be7cf2
Ahmed Nadar <ahmedfay@hotmail.com>
2014-10-20 23:39:58 -0400

    Update routes for settings and others

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
+  get 'settings/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   devise_scope :user do
 	  get "login", to: "devise/sessions#new", as: :login
 	  get "join", to: "registrations#new", as: :join
 	  get "logout", to: "devise/sessions#destroy", as: :logout
-	  get "settings", to: "registrations#edit", as: :settings
+	  get "profile", to: "registrations#edit", as: :profile
 	end
 
   resources :statuses
   get "feeds", to: "statuses#index", as: :feeds
   root "statuses#index"
+
+  get "/:id", to: "settings#show"
 end
